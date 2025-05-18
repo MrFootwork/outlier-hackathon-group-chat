ad4f1176bd677323841154b94726b96de9a51cfe
Pandau <pandau.ting@gmail.com>
2025-05-18 12:52:56 +0200

    extract TheNavigationDrawer to component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+import TheNavigationDrawer from './components/TheNavigationDrawer.vue'
 
-const theme = ref('light')
-const drawer = ref(true)
-
-function toggleTheme() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const theme = useTheme()
 </script>
 
 <template>
-  <v-app :theme="theme">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      permanent
-      :width="80"
-      color="highlight-dark"
-    >
-      <v-list class="d-flex flex-column align-center">
-        <v-list-item>
-          <v-btn
-            icon
-            variant="plain"
-            slim
-            @click="toggleTheme"
-          >
-            <v-icon :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" />
-          </v-btn>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app :theme="theme.global.name.value">
+    <TheNavigationDrawer />
 
     <v-main>
       <v-container>
---
+++ src/components/TheNavigationDrawer.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+
+const drawer = ref(true)
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+    permanent
+    :width="80"
+    color="highlight-dark"
+  >
+    <v-list class="d-flex flex-column align-center">
+      <v-list-item>
+        <v-btn
+          icon
+          variant="plain"
+          slim
+          @click="toggleTheme"
+        >
+          <v-icon
+            :icon="theme.global.name.value === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          />
+        </v-btn>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<style lang="scss" scoped></style>
