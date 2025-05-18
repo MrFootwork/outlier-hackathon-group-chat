63342fecec5c50d7fb7776cc72086c14f3363a63
Pandau <pandau.ting@gmail.com>
2025-05-18 13:30:24 +0200

    setup major app layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <script setup>
 import { useTheme } from 'vuetify'
 import TheNavigationDrawer from './components/TheNavigationDrawer.vue'
+import TheGroupChatSelector from './components/TheGroupChatSelector.vue'
+import TheChatDisplay from './components/TheChatDisplay.vue'
 
 const theme = useTheme()
 </script>
 
 <template>
-  <v-app :theme="theme.global.name.value">
+  <v-app
+    :theme="theme.global.name.value"
+    class="d-flex flex-column"
+  >
     <TheNavigationDrawer />
 
-    <v-main>
-      <v-container>
-        <h1>Main Content</h1>
-      </v-container>
-      <!-- <h1>Main Content</h1> -->
+    <v-main class="d-flex flex-row justify-space-between">
+      <TheGroupChatSelector class="width-40" />
+      <TheChatDisplay class="width-60" />
     </v-main>
   </v-app>
 </template>
 
-<style scoped></style>
+<style scoped>
+.width-40 {
+  width: 40%;
+}
+.width-60 {
+  width: 60%;
+}
+</style>
